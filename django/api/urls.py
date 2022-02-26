0a9672c4fa058331768c1e0561712a532acc8006
chengdh <cheng.donghui@gmail.com>
2022-02-26 14:57:28 +0000

    add get_permission api

--- django/api/urls.py
+++ django/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .github_login_view import GithubLoginView
-from .views import RoutersView
+from .views import RoutersView,PermissionsView
 urlpatterns = [
-    path('github/', GithubLoginView.as_view(), name='github_login'),
+    # path('github/', GithubLoginView.as_view(), name='github_login'),
     path('getAsyncRoutes/', RoutersView.as_view(), name='get_async_routes'),
+    path('getPermissions/', PermissionsView.as_view(), name='get_permissions'),
 ]
--- django/api/views.py
+++ django/api/views.py
@@ -1,6 +1,6 @@
-from rest_framework.generics import GenericAPIView, RetrieveAPIView
+from rest_framework.generics import GenericAPIView,ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .serializers import RoleSerializer 
+from .serializers import RoleSerializer,SystemFunctionOperateSerializer 
 from .models import Role
 
 
@@ -20,3 +20,16 @@ class RoutersView(RetrieveAPIView):
         django-rest-swagger
         """
         return Role.objects.none()
+
+class PermissionsView(ListAPIView):
+    """
+    获取当前用户前端权限
+    """
+    serializer_class = SystemFunctionOperateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """
+        得到角色权限
+        """
+        return self.request.user.get_default_role.system_function_operates.all()
