9a502a79776b1abb26f258af55117a1934f6674b
chengdh <cheng.donghui@gmail.com>
2022-03-12 02:01:23 +0000

    add customer and agent default

--- hasura/migrations/1647048298303_set_contractline_default/up.sql
+++ hasura/migrations/1647048298303_set_contractline_default/up.sql
@@ -14,9 +14,9 @@ ALTER TABLE electric_power_sale_contractline
     ALTER  price_common SET DEFAULT 0.0,
     ALTER  price_peak SET DEFAULT 0.0,
     ALTER  price_flat SET DEFAULT 0.0,
-    ALTER  price_valley SET DEFAULT 0.0;
-    ALTER  'state' SET DEFAULT "confirmed";
+    ALTER  price_valley SET DEFAULT 0.0,
+    ALTER  state SET DEFAULT 'confirmed';
 
 
 ALTER TABLE electric_power_sale_contract
-    ALTER  'state' SET DEFAULT "confirmed";
+    ALTER  state SET DEFAULT 'confirmed';
